35a92ebd108466dbc3aabdee7e6e3100688c6f09
Calvin <calvincruzader@gmail.com>
2019-02-11 16:42:29 -0600

    function reference lol

---
+++ function-notes.py
@@ -0,0 +1,4 @@
+## Array functions 
+
+my_arr.remove(2) # removes the first instance of 2 in an array
+my_arr.insert(3,999) # inserts 999 onto the 3rd index AND shifts everything else to the right by 1 
